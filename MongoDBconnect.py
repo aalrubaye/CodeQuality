c7462982f26fddbe2920827c6b5bb346732143ef
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2019-11-03 21:32:28 -0500

    Connection to reading to and from the database

--- MongoDBconnect.py
+++ MongoDBconnect.py
@@ -1,8 +1,17 @@
+import pprint
+
 from pymongo import MongoClient
 
 __author__ = 'Abdul Rubaye'
 
 client = MongoClient()
-database = client.githubData
-events = database.events
-final_db = database.final
+database = client.github_data
+repos = database.repos
+
+
+# The main function
+if __name__ == "__main__":
+
+    for e in repos.find():
+        pprint.pprint(e)
+        break
