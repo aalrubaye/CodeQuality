d2d324de9da513489127f87f62714f0b6a9a8dc1
Abdul Rubaye <aalrubaye2013@my.fit.edu>
2019-11-03 21:53:07 -0500

    function to fetch and attach client secret key

--- MongoDBconnect.py
+++ MongoDBconnect.py
@@ -8,6 +8,17 @@ client = MongoClient()
 database = client.github_data
 repos = database.repos
 
+# client id and client secret are used in calling the github API
+# they will help to raise the maximum limit of calls per hour
+# note: you will need your private txt file that includes the private keys
+privateVar = open("privateVar.txt",'r').read()
+client_id = privateVar.split('\n', 1)[0]
+client_secret = privateVar.split('\n', 1)[1]
+
+# appends the client id and the client secret to urls
+def add_client_id_client_secret_to_url(url,page):
+    query = '?per_page=100&page='+str(page)+'&client_id='+client_id+'&client_secret='+client_secret
+    return url+query
 
 # The main function
 if __name__ == "__main__":
